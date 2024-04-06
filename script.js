7c428f16d7cfbd4c917c68fe9caf6a1f7565687c
Kamran Zafar <kamranzafar4343@gmail.com>
2024-04-06 22:38:50 +0500

    feat - js function

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 const display = document.querySelector(".display");
-const button = document.querySelectorAll("button");
+const buttons = document.querySelectorAll("button");
+const specialChars = ["%", "+", "-", "=", "/", "*"];
+let output = "";
 
+
+//define function to calculate base on button click
+const calculate = (btnValue) => {
+    if(btnValue === "=" && btnValue !== ""){
+        //if output has % replace it with /
+        output = eval(output.replace("%", "/100"));
+    }
+    else if(btnValue==="AC"){
+        //all data in input field will be removed, when user click AC
+        output = "";
+    }
+    else if(btnValue==="DEL"){
+        //when user click DEL it deletes last one character
+        output= output.toString().slice(0, -1);
+    }
+
+    else{
+        if(btnValue === "" && specialChars.includes(btnValue)) return;
+         output += btnValue;
+    }
+    display.value=output;
+};
+
+
+//add event listener to buttons, call calculate on click
+buttons.forEach((button) => {
+    //button click listener calls calculate() with dataset value as argument
+    button.addEventListener("click", (e)=> calculate(e.target.dataset.value));   
+});
